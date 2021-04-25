3d8d007ca157c49e2190b4e33fa1f952278040ed
Ivan Asonov <asonovivan88@gmail.com>
2021-04-25 21:55:20 +0200

    Utility files

---
+++ cmake-build-debug/CMakeFiles/cpp_practice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_practice.dir/src/com/competetive/practice/leetcode/solutions/medium/M384ShuffleAnArray.cpp.o"
+  "cpp_practice"
+  "cpp_practice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
